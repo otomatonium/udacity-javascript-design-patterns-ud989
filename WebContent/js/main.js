61ddc88ae9c9ffd5a8372a7f28160eb922bc5f57
otomatonium <6641160+otomatonium@users.noreply.github.com>
2018-06-26 23:03:29 -0700

    Implement first requirements change: add another cat clicker

--- WebContent/js/main.js
+++ WebContent/js/main.js
@@ -1,9 +1,14 @@
 $(document).ready(function(){
 
 	var clickCount = $("#counter").val();
+	var clickCount2 = $("#counter2").val();
 
-	$(".image > img").click(function() {
+	$("img#cat").click(function() {
 		$("#counter").text(++clickCount);
 	});
+	
+	$("img#cat2").click(function() {
+		$("#counter2").text(++clickCount2);
+	});	
 
 });
